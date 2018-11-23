4426dbda1351065be59d82d82ae29924290963b8
Takashi Kokubun <takashikkbn@gmail.com>
2018-11-23 11:46:35 +0900

    Make profile configurable

--- profile.rb
+++ profile.rb
@@ -1,12 +1,14 @@
 require_relative 'app'
 require 'stackprof'
 
+requests = Integer(ENV.fetch('REQUESTS', '50000'))
+
 app = Rack::MockRequest.new(Sinatra::Application)
 path = '/'
 
 StackProf.run(mode: ENV.key?('WALL') ? :wall : :cpu, interval: Integer(ENV.fetch('INTERVAL', '1')), out: 'stackprof.dump') do
   i = 0
-  while i < 50000
+  while i < requests
     app.get(path)
     i += 1
   end
