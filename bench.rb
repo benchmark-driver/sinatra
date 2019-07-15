ba09f4ab1468876767cec0c16bd4c5bcbda97760
Takashi Kokubun <takashikkbn@gmail.com>
2019-07-15 10:51:13 +0900

    Add perf for sinatra

--- bench.rb
+++ bench.rb
@@ -18,6 +18,9 @@ if warmup > 0
 end
 
 # benchmark
+if use_perf = ENV.key?('PERF')
+  pid = Process.spawn('perf', 'stat', '-p', Process.pid.to_s, '-e', 'task-clock,cycles,instructions,branches,branch-misses,cache-misses,cache-references,l1d_pend_miss.pending_cycles,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_code_rd,l2_rqsts.code_rd_hit,dsb2mite_switches.penalty_cycles,icache.hit,icache.ifdata_stall,icache.ifetch_stall,icache.misses,idq.all_dsb_cycles_4_uops,idq.all_dsb_cycles_any_uops,idq.all_mite_cycles_4_uops,idq.all_mite_cycles_any_uops,idq.dsb_cycles,idq.dsb_uops,l2_rqsts.code_rd_hit,l2_rqsts.code_rd_miss')
+end
 i = 1
 time = 0.0
 while i <= requests
@@ -30,6 +33,10 @@ while i <= requests
   i += 1
 end
 puts
+if use_perf
+  Process.kill(:INT, pid)
+  Process.wait(pid)
+end
 
 # print result
 puts "#{"%.2f" % (requests.to_f / time)} rps"
