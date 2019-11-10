8be7f22779d3e75b907ccfa4d4b54295674fb63b
Takashi Kokubun <takashikkbn@gmail.com>
2019-11-10 12:37:04 -0800

    Pause before benchmark

--- bench.rb
+++ bench.rb
@@ -17,6 +17,10 @@ if warmup > 0
   puts
 end
 
+if RubyVM::MJIT.enabled?
+  RubyVM::MJIT.pause
+end
+
 # benchmark
 if use_perf = ENV.key?('PERF')
   pid = Process.spawn('perf', 'stat', '-p', Process.pid.to_s, '-e', 'task-clock,cycles,instructions,branches,branch-misses,cache-misses,cache-references,l1d_pend_miss.pending_cycles,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_code_rd,l2_rqsts.code_rd_hit,dsb2mite_switches.penalty_cycles,icache.hit,icache.ifdata_stall,icache.ifetch_stall,icache.misses,idq.all_dsb_cycles_4_uops,idq.all_dsb_cycles_any_uops,idq.all_mite_cycles_4_uops,idq.all_mite_cycles_any_uops,idq.dsb_cycles,idq.dsb_uops,l2_rqsts.code_rd_hit,l2_rqsts.code_rd_miss')
